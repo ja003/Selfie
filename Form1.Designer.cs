2cd094a872fdb38fcf1bced2013875193b5bc8de
Adam Jurík <ja004@seznam.cz>
2021-11-21 13:47:16 +0100

    face detection

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,83 @@ namespace Selfie1
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.detectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(734, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.detectToolStripMenuItem});
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+			this.openToolStripMenuItem.Text = "open";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+			this.fileToolStripMenuItem.Text = "file";
+			this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+			// 
+			// detectToolStripMenuItem
+			// 
+			this.detectToolStripMenuItem.Name = "detectToolStripMenuItem";
+			this.detectToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+			this.detectToolStripMenuItem.Text = "detect";
+			this.detectToolStripMenuItem.Click += new System.EventHandler(this.detectToolStripMenuItem_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(13, 47);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(700, 404);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(734, 490);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ToolStripMenuItem detectToolStripMenuItem;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,19 +3,82 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 namespace Selfie1
 {
 	public partial class Form1 : Form
 	{
+		Image<Bgr, byte> imgInput;
+
 		public Form1()
 		{
 			InitializeComponent();
 		}
 
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			imgInput = new Image<Bgr, byte>("D:\\Coding\\C#\\Selfie\\Selfie1\\test_imgs\\IMG_20211113_150047.jpg");
+			pictureBox1.Image = imgInput.AsBitmap();
+
+			DetectFaceHaar();
+		}
+
+		private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			if(dialog.ShowDialog() == DialogResult.OK)
+			{
+				imgInput = new Image<Bgr, byte>(dialog.FileName);
+				pictureBox1.Image = imgInput.AsBitmap();
+			}
+		}
+
+		private void detectToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if(imgInput == null)
+				{
+					throw new Exception("no image");
+				}
+				DetectFaceHaar();
+			}
+			catch(Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		public void DetectFaceHaar()
+		{
+			try
+			{
+				string facePath = Path.GetFullPath(@"../../../haar_data/haarcascade_frontalface_default.xml");
+				CascadeClassifier classifier = new CascadeClassifier(facePath);
+				var imgGray = imgInput.Convert<Gray, byte>().Clone();
+				var faces = classifier.DetectMultiScale(imgGray, 1.1, 4);
+				foreach(var face in faces)
+				{
+					imgInput.Draw(face, new Bgr(0, 0, 255), 2);
+				}
+
+				Bitmap bitmap = imgInput.AsBitmap();
+				bitmap.SetResolution(pictureBox1.Width, pictureBox1.Height);
+				pictureBox1.Image = bitmap;
+			}
+			catch(Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
 	}
 }
