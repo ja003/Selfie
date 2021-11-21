da403b2f4c36e3d0f646e8e9394f395cecbc09d3
Adam Jurík <ja004@seznam.cz>
2021-11-21 13:59:27 +0100

    eye detection

--- Form1.cs
+++ Form1.cs
@@ -26,10 +26,11 @@ namespace Selfie1
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			imgInput = new Image<Bgr, byte>("D:\\Coding\\C#\\Selfie\\Selfie1\\test_imgs\\IMG_20211113_150047.jpg");
+			imgInput = new Image<Bgr, byte>("D:\\Coding\\C#\\Selfie\\Selfie1\\test_imgs\\IMG_20211113_150047_resize_1920.jpg");
 			pictureBox1.Image = imgInput.AsBitmap();
 
-			DetectFaceHaar();
+			//DetectFaceHaar();
+			DetectEyesHaar();
 		}
 
 		private void fileToolStripMenuItem_Click(object sender, EventArgs e)
@@ -51,6 +52,34 @@ namespace Selfie1
 					throw new Exception("no image");
 				}
 				DetectFaceHaar();
+			}
+			catch(Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		public void DetectEyesHaar()
+		{
+			try
+			{
+				string facePath = Path.GetFullPath(@"../../../haar_data/haarcascade_eye.xml");
+				CascadeClassifier classifier = new CascadeClassifier(facePath);
+				var imgGray = imgInput.Convert<Gray, byte>().Clone();
+				Size averageEyeSize = new Size(200, 150);
+				Size minEyeSize = new Size(150, 100);
+				Size maxEyeSize = new Size(250, 200);
+				var eyes = classifier.DetectMultiScale(imgGray, 1.1,3, minEyeSize, maxEyeSize);
+				foreach(var eye in eyes)
+				{
+					imgInput.Draw(eye, new Bgr(0, 0, 255), 4);
+					Cross2DF eyeCenter = new Cross2DF(new PointF(eye.X + eye.Width/2, eye.Y+eye.Height/2), 20,20);
+					imgInput.Draw(eyeCenter, new Bgr(255, 0, 0), 4);
+				}
+
+				Bitmap bitmap = imgInput.AsBitmap();
+				bitmap.SetResolution(pictureBox1.Width, pictureBox1.Height);
+				pictureBox1.Image = bitmap;
 			}
 			catch(Exception ex)
 			{
