d93648819f55cb3feb4755aa446e10627e9ee524
Adam Jurík <ja004@seznam.cz>
2021-11-21 18:13:46 +0100

    2 pupils success

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,12 +38,20 @@ namespace Selfie1
 			this.pictureBoxEye2 = new System.Windows.Forms.PictureBox();
 			this.pictureBoxEye2_a = new System.Windows.Forms.PictureBox();
 			this.pictureBoxEye1_a = new System.Windows.Forms.PictureBox();
+			this.pictureBoxEye2_b = new System.Windows.Forms.PictureBox();
+			this.pictureBoxEye1_b = new System.Windows.Forms.PictureBox();
+			this.pictureBoxEye2_c = new System.Windows.Forms.PictureBox();
+			this.pictureBoxEye1_c = new System.Windows.Forms.PictureBox();
 			this.menuStrip1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye2)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye2_a)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye1_a)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye2_b)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye1_b)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye2_c)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye1_c)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// menuStrip1
@@ -83,14 +91,14 @@ namespace Selfie1
 			// 
 			this.pictureBox1.Location = new System.Drawing.Point(13, 47);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(640, 360);
+			this.pictureBox1.Size = new System.Drawing.Size(434, 280);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 1;
 			this.pictureBox1.TabStop = false;
 			// 
 			// pictureBoxEye1
 			// 
-			this.pictureBoxEye1.Location = new System.Drawing.Point(13, 429);
+			this.pictureBoxEye1.Location = new System.Drawing.Point(12, 333);
 			this.pictureBoxEye1.Name = "pictureBoxEye1";
 			this.pictureBoxEye1.Size = new System.Drawing.Size(208, 106);
 			this.pictureBoxEye1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -99,7 +107,7 @@ namespace Selfie1
 			// 
 			// pictureBoxEye2
 			// 
-			this.pictureBoxEye2.Location = new System.Drawing.Point(239, 429);
+			this.pictureBoxEye2.Location = new System.Drawing.Point(238, 333);
 			this.pictureBoxEye2.Name = "pictureBoxEye2";
 			this.pictureBoxEye2.Size = new System.Drawing.Size(208, 106);
 			this.pictureBoxEye2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -108,7 +116,7 @@ namespace Selfie1
 			// 
 			// pictureBoxEye2_a
 			// 
-			this.pictureBoxEye2_a.Location = new System.Drawing.Point(239, 550);
+			this.pictureBoxEye2_a.Location = new System.Drawing.Point(238, 449);
 			this.pictureBoxEye2_a.Name = "pictureBoxEye2_a";
 			this.pictureBoxEye2_a.Size = new System.Drawing.Size(208, 106);
 			this.pictureBoxEye2_a.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -117,18 +125,58 @@ namespace Selfie1
 			// 
 			// pictureBoxEye1_a
 			// 
-			this.pictureBoxEye1_a.Location = new System.Drawing.Point(13, 550);
+			this.pictureBoxEye1_a.Location = new System.Drawing.Point(12, 449);
 			this.pictureBoxEye1_a.Name = "pictureBoxEye1_a";
 			this.pictureBoxEye1_a.Size = new System.Drawing.Size(208, 106);
 			this.pictureBoxEye1_a.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBoxEye1_a.TabIndex = 4;
 			this.pictureBoxEye1_a.TabStop = false;
 			// 
+			// pictureBoxEye2_b
+			// 
+			this.pictureBoxEye2_b.Location = new System.Drawing.Point(238, 561);
+			this.pictureBoxEye2_b.Name = "pictureBoxEye2_b";
+			this.pictureBoxEye2_b.Size = new System.Drawing.Size(208, 106);
+			this.pictureBoxEye2_b.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBoxEye2_b.TabIndex = 7;
+			this.pictureBoxEye2_b.TabStop = false;
+			// 
+			// pictureBoxEye1_b
+			// 
+			this.pictureBoxEye1_b.Location = new System.Drawing.Point(12, 561);
+			this.pictureBoxEye1_b.Name = "pictureBoxEye1_b";
+			this.pictureBoxEye1_b.Size = new System.Drawing.Size(208, 106);
+			this.pictureBoxEye1_b.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBoxEye1_b.TabIndex = 6;
+			this.pictureBoxEye1_b.TabStop = false;
+			// 
+			// pictureBoxEye2_c
+			// 
+			this.pictureBoxEye2_c.Location = new System.Drawing.Point(239, 673);
+			this.pictureBoxEye2_c.Name = "pictureBoxEye2_c";
+			this.pictureBoxEye2_c.Size = new System.Drawing.Size(208, 106);
+			this.pictureBoxEye2_c.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBoxEye2_c.TabIndex = 9;
+			this.pictureBoxEye2_c.TabStop = false;
+			// 
+			// pictureBoxEye1_c
+			// 
+			this.pictureBoxEye1_c.Location = new System.Drawing.Point(13, 673);
+			this.pictureBoxEye1_c.Name = "pictureBoxEye1_c";
+			this.pictureBoxEye1_c.Size = new System.Drawing.Size(208, 106);
+			this.pictureBoxEye1_c.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBoxEye1_c.TabIndex = 8;
+			this.pictureBoxEye1_c.TabStop = false;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(734, 773);
+			this.ClientSize = new System.Drawing.Size(734, 862);
+			this.Controls.Add(this.pictureBoxEye2_c);
+			this.Controls.Add(this.pictureBoxEye1_c);
+			this.Controls.Add(this.pictureBoxEye2_b);
+			this.Controls.Add(this.pictureBoxEye1_b);
 			this.Controls.Add(this.pictureBoxEye2_a);
 			this.Controls.Add(this.pictureBoxEye1_a);
 			this.Controls.Add(this.pictureBoxEye2);
@@ -146,6 +194,10 @@ namespace Selfie1
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye2)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye2_a)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye1_a)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye2_b)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye1_b)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye2_c)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye1_c)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -162,6 +214,10 @@ namespace Selfie1
 		private System.Windows.Forms.PictureBox pictureBoxEye2;
 		private System.Windows.Forms.PictureBox pictureBoxEye2_a;
 		private System.Windows.Forms.PictureBox pictureBoxEye1_a;
+		private System.Windows.Forms.PictureBox pictureBoxEye2_b;
+		private System.Windows.Forms.PictureBox pictureBoxEye1_b;
+		private System.Windows.Forms.PictureBox pictureBoxEye2_c;
+		private System.Windows.Forms.PictureBox pictureBoxEye1_c;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -70,7 +70,7 @@ namespace Selfie1
 				Size averageEyeSize = new Size(200, 150);
 				Size minEyeSize = new Size(150, 100);
 				Size maxEyeSize = new Size(250, 200);
-				var eyes = classifier.DetectMultiScale(imgGray, 1.1,3, minEyeSize, maxEyeSize);
+				var eyes = classifier.DetectMultiScale(imgGray, 1.1, 3, minEyeSize, maxEyeSize);
 
 				if(eyes.Length != 2)
 				{
@@ -99,23 +99,42 @@ namespace Selfie1
 			Image<Gray, byte> eyeImg = pSourceImg.Copy();
 
 
+			//EYE
 			PictureBox pictureBoxEye = pEyeIndex == 0 ? pictureBoxEye1 : pictureBoxEye2;
 			pictureBoxEye.Image = eyeImg.AsBitmap();
 
-			eyeImg.ca
+			//THRESHOLD
+			var threshold = eyeImg.ThresholdToZeroInv(new Gray(40));
+			PictureBox pictureBoxEye_a = pEyeIndex == 0 ? pictureBoxEye1_a : pictureBoxEye2_a;
+			pictureBoxEye_a.Image = threshold.AsBitmap();
 
-			var circles = eyeImg.HoughCircles(new Gray(50), new Gray(50), 2, 1, 15, 25);
+			//CANNY
+			const int cannyThreshold = 30;
+			var canny = threshold.Canny(cannyThreshold, 2);
+			PictureBox pictureBoxEye_b = pEyeIndex == 0 ? pictureBoxEye1_b : pictureBoxEye2_b;
+			pictureBoxEye_b.Image = canny.AsBitmap();
+
+			//HOUGH CIRCLES
+			const int minRadius = 7;
+			const int maxRadius = 12;
+			const int accumulatorThreshold = 25;
+			const int dp = 2;
+			const int minDist = 5;
+			var circlesChannels = threshold.HoughCircles(new Gray(cannyThreshold), new Gray(accumulatorThreshold), dp, minDist, minRadius, maxRadius);
 
 			var pupil = eyeImg.Copy();
-			foreach(var circle in circles) {
-				pupil.Draw(circle[0], new Gray(255));
-				Console.WriteLine(circle[0].Center.X);
+			foreach(var circleChannel in circlesChannels)
+			{
+				foreach(var circle in circleChannel)
+				{
+					pupil.Draw(circle, new Gray(255));
+					Console.WriteLine(circle.Center.X);
+				}
 			}
 
 
-			//var pupil = eyeImg.ThresholdToZeroInv(new Gray(40));
-			PictureBox pictureBoxEye_a = pEyeIndex == 0 ? pictureBoxEye1_a : pictureBoxEye2_a;
-			pictureBoxEye_a.Image = pupil.AsBitmap();
+			PictureBox pictureBoxEye_c = pEyeIndex == 0 ? pictureBoxEye1_c : pictureBoxEye2_c;
+			pictureBoxEye_c.Image = pupil.AsBitmap();
 
 
 			return new Point();
