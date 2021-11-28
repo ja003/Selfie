96857415c311fb643f38b3f1fb5cfb28c30b57e8
Adam Jurík <ja004@seznam.cz>
2021-11-28 11:56:03 +0100

    - affine transformation

--- ManualInput.cs
+++ ManualInput.cs
@@ -3,6 +3,7 @@ using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,34 @@ namespace Selfie1
 {
 	class ManualInput
 	{
+		Image<Bgr, byte> inputImage;
+
 		private PictureBox pictureBox_Input;
 		private PictureBox pictureBox_Output;
+
+		PointF input_eyeLeft;
+		PointF input_eyeRight;
+
+		PointF output_eyeLeft; // 836/1920
+		PointF output_eyeRight; // 1086/1920
+
 
 		public ManualInput(PictureBox pictureBox_Input, PictureBox pictureBox_Output)
 		{
 			this.pictureBox_Input = pictureBox_Input;
 			this.pictureBox_Output = pictureBox_Output;
+		}
+
+
+		internal void SetInput(Image<Bgr, byte> image)
+		{
+			double scale = 1920f / image.Width;
+			inputImage = image.Resize(scale, Emgu.CV.CvEnum.Inter.Linear);
+
+			pictureBox_Input.Image = inputImage.AsBitmap();
+
+			//debug
+			OnClick_Apply();
 		}
 
 		internal void OnClick_Input(MouseEventArgs mouseEventArgs)
@@ -28,16 +50,29 @@ namespace Selfie1
 
 		internal void OnClick_Apply()
 		{
-			pictureBox_Output.Image = pictureBox_Input.Image;
+			//debug
+			input_eyeLeft = new PointF(186, 138);
+			input_eyeRight = new PointF(247, 139);
+
+			int outputLeftX = (int)(836f / 1920 * pictureBox_Output.Size.Width);
+			int outputRightX = (int)(1086f / 1920 * pictureBox_Output.Size.Width);
+			int outputY = pictureBox_Output.Size.Height / 2;
+			output_eyeLeft = new PointF(outputLeftX, outputY);
+			output_eyeRight = new PointF(outputRightX, outputY);
+
+			ApplyTransform();
+
+			pictureBox_Output.Image = inputImage.AsBitmap();
 		}
 
-		internal void SetInput(Image<Bgr, byte> image)
+		private void ApplyTransform()
 		{
-			double scale = 1920f / image.Width;
-			image = image.Resize(scale, Emgu.CV.CvEnum.Inter.Linear);
+			PointF[] src = new PointF[] { input_eyeLeft, input_eyeRight, input_eyeRight };
+			PointF[] dest = new PointF[] { output_eyeLeft, output_eyeRight, output_eyeRight };
+			Mat affineMat = CvInvoke.GetAffineTransform(src, dest);
 
-			pictureBox_Input.Image = image.AsBitmap();
-
+			inputImage.WarpAffine(affineMat, Emgu.CV.CvEnum.Inter.Linear, Emgu.CV.CvEnum.Warp.Default, Emgu.CV.CvEnum.BorderType.Default, new Bgr(255, 255, 255));
 		}
+
 	}
 }
